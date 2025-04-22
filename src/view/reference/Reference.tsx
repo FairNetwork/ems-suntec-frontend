1decd8bdc57ee541c627109021544b756c49e6ee
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-04-22 12:52:59 +0200

    ✨ Add reference

--- src/view/reference/Reference.tsx
+++ src/view/reference/Reference.tsx
@@ -2,6 +2,7 @@ import './reference.scss';
 import { useReferenceId } from '../../hooks/route';
 import { useReference } from '../../hooks/references';
 import { useNavigate } from 'react-router-dom';
+import Icon from '../../components/shared/icon/Icon';
 
 const Reference = () => {
     const navigate = useNavigate();
@@ -11,9 +12,37 @@ const Reference = () => {
 
     if (!reference) {
         navigate('no_content');
+
+        return null;
     }
 
-    return <div className="reference">TEST</div>;
+    const { title, details, image, location, description } = reference;
+
+    return (
+        <div className="reference">
+            <div className="reference__image">
+                <img src={image} alt={title} />
+                <div className="reference__image__location">
+                    <Icon icon="fas fa-location-dot" />
+                    <div className="reference__image__location__text">{location}</div>
+                </div>
+            </div>
+            <h1>{title}</h1>
+            <div className="reference__description">{description}</div>
+            <div className="reference__table">
+                <table>
+                    <tbody>
+                    {details.map((detail, index) => (
+                        <tr key={index}>
+                            <td>{detail.label}</td>
+                            <td>{detail.value}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 Reference.displayName = 'Reference';
