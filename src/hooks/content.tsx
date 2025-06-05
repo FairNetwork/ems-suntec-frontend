feaecd60c66996bd599d801b08787be7cf69a409
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-06-05 08:09:05 +0200

    🔥 Removes unused contact route
    Removes the contact route and related component
    from the application as it is no longer needed.

--- src/hooks/content.tsx
+++ src/hooks/content.tsx
@@ -5,7 +5,6 @@ import NoContent from '../view/no-content/NoContent';
 import Utility from '../view/utility/Utility';
 import References from '../view/references/References';
 import Reference from '../view/reference/Reference';
-import Contact from '../view/contact/Contact';
 
 export const useContent = () => {
     const location = useLocation();
@@ -21,7 +20,7 @@ export const useContent = () => {
 
         if (path.startsWith('/utility')) return <Utility />;
 
-        if (path === '/contact') return <Contact />;
+        // if (path === '/contact') return <Contact />;
 
         return <NoContent />;
     }, [location.pathname]);
